ec9bcf48fdc950d16448fd3b5842affd7ac9143d
Rodney Coleman <rc6886@gmail.com>
2023-10-06 15:44:54 -0500

    add-mobile: Added Ionic Capacitor for iOS.

---
+++ UI/Web/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kavita',
+  appName: 'kavita-mobile',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
